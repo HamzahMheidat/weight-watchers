191820103f43b1b72fde86b5b096c1ae1c259f15
HamzahMheidat <hamzahmheidat96@github.com>
2019-09-03 17:45:42 +0300

    fixs

--- find-studio/find-studio.specs.js
+++ find-studio/find-studio.specs.js
@@ -5,20 +5,20 @@ const utilityFunctions = require('../utility-Functions /array-utility.js');
 describe('Test find studio at weight watchers website', () => {
     const zipCode = '10003';
 
-    it(`Should verify that url contains the zip code from search field`, async() => {
+    it(`should verify that url contains the zip code from search field`, async() => {
         await findStudioPageObjects.navigateToFindStudio();
         await findStudioPageObjects.searchStudio(zipCode);
         expect(await browser.getCurrentUrl()).toContain(zipCode);
     });
 
-    it(`Should verify that first location displayed contains the zip code from search field`, async() => {
+    it(`should verify that first location displayed contains the zip code from search field`, async() => {
         await findStudioPageObjects.navigateToFindStudio();
         await findStudioPageObjects.searchStudio(zipCode);
         const meetingLocations = await findStudioPageObjects.getElementAttribute(findStudioLocators.locations, 'innerText');
         expect(meetingLocations).toContain(zipCode);
     });
 
-    it(`Should verify that the location appears when filtering by days in the location schedule`, async() => {
+    it(`should verify that the location appears when filtering by days in the location schedule`, async() => {
         findStudioPageObjects.navigateToFindStudio();
         await findStudioPageObjects.searchStudio(zipCode);
         const firstLocationId = await findStudioPageObjects.getElementAttribute(findStudioLocators.meetingLocationDivs, 'id');
@@ -29,7 +29,7 @@ describe('Test find studio at weight watchers website', () => {
         expect(firstLocationId == firstLocationIdAfterClicking).toBe(true);
     });
 
-    it(`Should verify that the first location disappears when filtering by days out of the location schedule`, async() => {
+    it(`should verify that the first location disappears when filtering by days out of the location schedule`, async() => {
         findStudioPageObjects.navigateToFindStudio();
         await findStudioPageObjects.searchStudio(zipCode);
         const firstLocationId = await findStudioPageObjects.getElementAttribute(findStudioLocators.meetingLocationDivs, 'id');
