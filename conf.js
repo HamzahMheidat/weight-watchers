8365277dfe6daef53427629c1cd2c3a6a5d323b4
HamzahMheidat <hamzahmheidat96@github.com>
2019-09-04 10:47:41 +0300

    fixs

--- conf.js
+++ conf.js
@@ -1,11 +1,11 @@
 var { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
-    directConnect: true,
+    seleniumAddress: 'http://localhost:4444/wd/hub',
     capabilities: {
         browserName: 'chrome',
         chromeOptions: {
-            args: ["--window-size=2000,1500",
+            args: ["--window-size=1800,1200",
                 "--headless"
             ]
         }
@@ -14,7 +14,7 @@ exports.config = {
     specs: ['find-studio/find-studio.specs.js'],
     onPrepare: () => {
         global.EC = protractor.ExpectedConditions;
-        global.WAIT_DURATION = 5000;
+        global.WAIT_DURATION = 20000;
         global.WEEK_DAYS = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
         global.BASE_URL = 'https://www.weightwatchers.com/us/';
         browser.waitForAngularEnabled(false);
